1d613bf6a35f182745e85725e22b3c98f40526cb
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-02 11:30:02 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,11 @@
+def menu():
+    print('-----Bem vindo ao sistema acadêmico do IFMS Três Lagoas-----')
+    print('[1] Cadastro de alunos') 
+    print('[2] Cadastro de professor') 
+    print('[3] Adicionar nova disciplia') 
+    print('[4] Adicionar nova turma') 
+    print('[5] Consultar alunos matrículados') 
+    print('[6] Consultar turmas abertas') 
+    print('[7] Consultar professores efetivados')
+    option = int(input('Informe a opção desejada:  '))
+menu() 
