a5ea7637749228b0d24eba378e97cff6f2035c2d
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-05 17:56:10 -0400

    Update cadastro_alunos.py

--- cadastro_alunos.py
+++ cadastro_alunos.py
@@ -1,25 +1,37 @@
+import random
 def cadastrar_alunos():
-    import random
-    class_student = ['1 ° ano A', '1° ano B', '2 ano A', '2° ano B', '3° ano A', '3° ano B']
-    print('-----Dados pessoais-----')
-    name = str(input('Nome do aluno:  '))
-    last_name = str(input('Sobrenome:  '))
-    age = int(input('Idade:  '))
-    print('-----Dados acadêmicos-----')
-    registration = random.randint(1000,10000)
-    classes = str(input('Informe a turma:  '))
-    print('-----Contato-----')
-    address = input('Endereço:  ')
-    email = input('Email:  ')
-    phone_number = input('Telefone:  ')
-    print('-----------------------------------')
-    print('Aluno cadastrado com sucesso!')
-    print(name, last_name)
-    print(f'{age} anos')
-    print(registration, classes)
-    print(address)
-    print(email)
-    print(phone_number)
+    while True:
+        alunos = []
+        print('-----Dados pessoais-----')
+        name = str(input('Nome do aluno:  '))
+        last_name = str(input('Sobrenome:  '))
+        age = int(input('Idade:  '))
+        print('-----Dados acadêmicos-----')
+        registration = random.randint(1000,10000)
+        classes = str(input('Informe a turma:  '))
+        print('-----Contato-----')
+        address = input('Endereço:  ')
+        email = input('Email:  ')
+        phone_number = input('Telefone:  ')
+    #dicionário com dados do aluno
+        aluno = {'Nome': name,
+             'Sobrenome': last_name,
+             'Idade': age,
+             'Matrícula': registration,
+             'Turma': classes,
+             'Endereço': address,
+             'email': email,
+             'Telefone': phone_number,}
+        alunos.append(aluno)
+        print('-----------------------------------')
+        print('Aluno cadastrado com sucesso!')
+        print(alunos)
+        resp = str(input('Deseja cadastrar outro aluno? [SIM/NÃO]:  '))
+        if resp.upper() == 'SIM':
+            print(cadastrar_alunos())
+        elif resp.upper() == 'NÃO':
+            print('Retornando ao menu principal...')
+        break
 cadastrar_alunos()
     
     
