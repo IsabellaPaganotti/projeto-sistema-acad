d24bfce2dd01d31fcfbb910fb29fd18033464e65
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-05 19:26:30 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,21 +1,26 @@
 def menu():
     print('-----Bem vindo ao sistema acadêmico do IFMS Três Lagoas-----')
-    print('[1] Cadastro de alunos') 
-    print('[2] Cadastro de professor') 
-    print('[3] Adicionar nova disciplia') 
-    print('[4] Adicionar nova turma') 
-    print('[5] Consultar alunos matrículados') 
-    print('[6] Consultar turmas abertas') 
+    print('[1] Cadastro de alunos')
+    print('[2] Cadastro de professor')
+    print('[3] Adicionar nova disciplina')
+    print('[4] Adicionar nova turma')
+    print('[5] Consultar alunos matriculados')
+    print('[6] Consultar turmas abertas')
     print('[7] Consultar professores efetivados')
-    option = int(input('Informe a opção desejada:  '))
-menu() 
+    return int(input('Informe a opção desejada: '))
 
-import cadastro_alunos
-def opcoes():
-    option = menu()
+def opcoes_principal():
     while True:
-        match option:
-            case '1':
-                print(cadastro_alunos.cadastrar_alunos())
-        break
-opcoes()
+        option = menu()
+        if option == 1:
+            import cadastro_alunos
+            cadastro_alunos.cadastrar_alunos()
+        elif option == 2:
+            import cadastro_professor
+            cadastro_professor.cadastrar_professores()
+        else:
+            print("Opção inválida.")
+        resposta = input("Deseja continuar no menu? (SIM/NÃO): ")
+        if resposta.upper() != "SIM":
+            break
+opcoes_principal()
