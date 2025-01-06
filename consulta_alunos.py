f80c584dd299ef77767cbf4e55cafd58ac93a1be
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-06 18:51:31 -0400

    Update consulta_alunos.py

--- consulta_alunos.py
+++ consulta_alunos.py
@@ -1,8 +1,12 @@
 from faker import Faker
 import random
+from datetime import date, timedelta
 fake = Faker(['pt_BR'])
 perfil = fake.profile()
 sexo = perfil['sex']
+idade_desejada = fake.random_int(min=15, max=25)
+nascimento = fake.date_of_birth()
+date_birthday = date.today().year - timedelta(days=idade_desejada*365)
 def consulta_de_alunos():
     print('-----CONSULTA DE ALUNOS MATRICULADOS-----')
     turmas = ['1° ano A', '1° ano B', '2° ano A', '2° ano B', '3° ano A', '3° ano B']
@@ -10,8 +14,9 @@ def consulta_de_alunos():
         print('-----DADOS PESSOAIS-----')
         print(f'Nome do aluno(a):  {fake.first_name()}')
         print(f'Sobrenome: {fake.last_name()}')
-        print(f'Idade {fake.random_int(min=15, max=25)} anos')
-        print(sexo)
+        print(f'Data de nascimento: {date_birthday.strftime('%d/%m/%y')}')
+        print(f'Idade {idade_desejada} anos')
+        print(f' Sexo: {sexo}')
         print('-----DADOS ACADÊMICOS-----')
         print(f'Turma: {fake.random_element(turmas)}')
         print(f'Matrícula: {random.randint(1000,10000)}')
