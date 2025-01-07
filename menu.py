995feaf6a251693c6ef1133a938e23466a97bd0f
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-07 14:40:30 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -12,25 +12,25 @@ def menu():
 def opcoes_principal():
     while True:
         option = menu()
-        if option == 1:
+        if option == 1: #cadastra novos alunos
             import cadastro_alunos
             cadastro_alunos.cadastrar_alunos()
         elif option == 2:
-            import cadastro_professor
+            import cadastro_professor #cadastra novos professores
             cadastro_professor.cadastrar_professores()
-        elif option == 3:
+        elif option == 3: #cadastra nova disciplia
             import novadisc
             novadisc.nova_disciplina()
-        elif option == 4:
+        elif option == 4: #cadastra nova turma
             import novaturma
             novaturma.nova_turma()
-        elif option == 5:
+        elif option == 5: #visualiza as turmas para onde foram os novos alunos cadastrados
             import cadastro_alunos
             cadastro_alunos.visualizador()
-        elif option == 6:
-            import turma_aberta
+        elif option == 6: #visualiza alunos com a biblioteca faker matriculados em turmas
+            import turma_aberta 
             turma_aberta.Visualizar_alunos_matriculados()
-        elif option == 7:
+        elif option == 7: #consulta professores efetivados com biblioteca faker
             import consulta_professor
             consulta_professor.consulta_professores()
         else:
