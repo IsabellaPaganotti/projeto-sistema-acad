ebb5a368f4fef5eb852f0346b4dc74f252b07048
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-07 01:38:51 -0400

    Update cadastro_alunos.py

--- cadastro_alunos.py
+++ cadastro_alunos.py
@@ -2,32 +2,31 @@ import random
 from faker import Faker
 fake = Faker()
 id = fake.bothify(text='#??###')
-turma1A = []
+turmas = {'1A': [], '2A': [], '3A': []}
+
 def cadastrar_alunos():
     print('-----Cadastro de alunos-----')
     alunos = []
     while True:
+        #informações pessoais
         print('-----Dados pessoais-----')
         nome = str(input('Nome do aluno: '))
         sobrenome = str(input('Sobrenome: '))
         idade = int(input('Idade: '))
         sexo = str(input('Sexo [Fem/Masc]: '))
-
+        #informações acadêmicas
         print('-----Dados acadêmicos-----')
         matricula = id
-        turma = str(input('Informe a turma: '))
-        if turma == '1° ano A':
-              alunos_turma1A = {'Aluno1': nome,
-                          'matrícula': id}
-              turma1A.append(alunos_turma1A)
-        else: 
-            print('erro')
+        turma = (input('Informe a turma: '))
+        aluno = {'Nome': nome, 'Sobrenome': sobrenome, 'Matricula': id}
+        turmas[turma].append(aluno)
+        #informações para contato
         print('-----Contato-----')
         endereco = input('Endereço: ')
         email = input('Email: ')
         telefone = input('Telefone: ')
         
-
+        #dicionário para conferir dados do aluno
         aluno = {
             'Nome': nome,
             'Sobrenome': sobrenome,
@@ -41,12 +40,17 @@ def cadastrar_alunos():
         }
       
         print('-----------------------------------')
-        print('Aluno cadastrado com sucesso!')
-        print(alunos)
-        print(f'turma: {turma1A}')
+        print(f'aluno {nome} cadastrado com sucesso na turma {turma}')
         resposta = input("Deseja cadastrar outro aluno? (SIM/NÃO): ")
         if resposta.upper() != "SIM":
             return
-
-
-    
+#função para visualizar alunos matrículados em turmas
+def visualizador():
+    turma = input('turma:  ')
+    if turma in turmas:
+        print(f'alunos na turma {turma}: ')
+        for aluno in turmas[turma]:
+            print(f'Aluno(a): {aluno['nome']}, Matrícula: {aluno['matricula']}')
+    else:
+        print('erro')
+    return
