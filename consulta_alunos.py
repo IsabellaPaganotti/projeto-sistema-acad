b22dc2c15acc448a32f0270a868c2cb951976b5b
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-07 14:40:45 -0400

    Update consulta_alunos.py

--- consulta_alunos.py
+++ consulta_alunos.py
@@ -6,9 +6,10 @@ perfil = fake.profile()
 sexo = perfil['sex']
 idade_desejada = fake.random_int(min=15, max=25)
 data_nascimento = date.today() - timedelta(days=idade_desejada*365)
+#função com dados falsos para alunos
 def consulta_de_alunos():
     print('-----CONSULTA DE ALUNOS MATRICULADOS-----')
-    turmas = ['1° ano A', '1° ano B', '2° ano A', '2° ano B', '3° ano A', '3° ano B']
+    turmas = ['1A', '2A', '3A',]
     while True:
         print('-----DADOS PESSOAIS-----')
         print(f'Nome do aluno(a):  {fake.first_name()}')
@@ -18,7 +19,7 @@ def consulta_de_alunos():
         print(f' Sexo: {sexo}')
         print('-----DADOS ACADÊMICOS-----')
         print(f'Turma: {fake.random_element(turmas)}')
-        print(f'Matrícula: {random.randint(1000,10000)}')
+        print(f'Matrícula: {fake.bothify(text='#??###')}')
         print('-----CONTATO-----')
         print(f'Email: {fake.email()}')
         print(f'Telefone: {fake.phone_number()}')
