791c59b291572013d77d63b4c9bbc9ff9b929595
IsabellaPaganotti <isabella.silva6@estudante.ifms.edu.br>
2025-01-06 23:02:36 -0400

    Update consulta_professor.py

--- consulta_professor.py
+++ consulta_professor.py
@@ -11,15 +11,23 @@ id = fake.bothify(text='#??###')
 def consulta_professores():
     disciplinas = ['Português', 'Matemática', 'História', 'Geografia', 'Artes', 'Filosfia', 'Sociologia',
                    'Química', 'Biologia', 'Física', 'Ed.Física', 'Inglês']
-    print('-----PROFESSORES EFETIVADOS-----')
-    print('-----DADOS PESSOAIS-----')
-    print(f'Professor(a): {fake.name()}')
-    print(f'Sexo: {sexo}')
-    print(f'Data de nascimento: {data_nascimento.strftime('%d/%m/%y')}')
-    print(f'Idade: {idade_desejada} anos')
-    print('-----DADOS PROFISSIONAIS-----')
-    print(f'Disciplina: {fake.random_element(disciplinas)} ')
-    print(f'ID: {id}')
+    while True:
+        print('-----PROFESSORES EFETIVADOS-----')
+        print('-----DADOS PESSOAIS-----')
+        print(f'Professor(a): {fake.name()}')
+        print(f'Sexo: {sexo}')
+        print(f'Data de nascimento: {data_nascimento.strftime('%d/%m/%y')}')
+        print(f'Idade: {idade_desejada} anos')
+        print('-----DADOS PROFISSIONAIS-----')
+        print(f'Disciplina: {fake.random_element(disciplinas)} ')
+        print(f'ID: {id}')
+        print('-----CONTATO-----')
+        print(f'Endereço: {fake.address()}')
+        print(f'Email: {fake.email()}')
+        print(f'Telefone: {fake.phone_number()}')
+        resp = input('Deseja consultar outro professor? [SIM/NÃO]:  ')
+        if resp.upper() != 'SIM':
+            break
     return
 
 
